7d69712a351b5c39710e9435d16ce0634c54594c
Yadier Abel de Quesada <yadier.quesada@etecsa.cu>
2017-12-21 10:38:35 -0500

    precio btc en json local

--- bitcoins/bitcoins.py
+++ bitcoins/bitcoins.py
@@ -2,6 +2,8 @@ import json
 #preciobtc = json.loads("https://api.coindesk.com/v1/bpi/currentprice.json")
 preciobtc = json.loads(open('bitcoins/bitcoins.json').read())
 
+
+#curl -d '{"result":{"action":"comandante.preguntar.preciobtc"}}' http://localhost:5000/webhook
 def devuelve_precio_actual(accion):
     if accion == "comandante.preguntar.preciobtc":
         speech = preciobtc
