ada25e31243360df44a3c8a6bdec72af5408f686
Yadier Abel de Quesada <yadier.quesada@etecsa.cu>
2017-12-21 13:09:22 -0500

    modificaciones a bitcoins

--- bitcoins/bitcoins.py
+++ bitcoins/bitcoins.py
@@ -2,10 +2,8 @@ import json
 #preciobtc = json.loads("https://api.coindesk.com/v1/bpi/currentprice.json")
 preciobtc = json.loads(open('bitcoins/bitcoins.json').read())
 
-
-#curl -d '{"result":{"action":"comandante.preguntar.preciobtc"}}' http://localhost:5000/webhook
 def devuelve_precio_actual(accion):
-    if accion == "comandante.preguntar.preciobtc":
+    if accion == "preguntar.preciobtc":
         speech = preciobtc
     return speech
 
