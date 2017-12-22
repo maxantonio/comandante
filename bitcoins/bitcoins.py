278ba51426b3f041a38ae8f4176af1165518c525
Yadier Abel de Quesada <yadier.quesada@etecsa.cu>
2017-12-22 12:33:44 -0500

    bitcoins

--- bitcoins/bitcoins.py
+++ bitcoins/bitcoins.py
@@ -3,18 +3,20 @@ import json
 from datetime import datetime
 
 def devuelve_precio_actual(accion,parameters):
-    url = "http://api.coinmarketcap.com/v1/ticker/"
-    speech = {}
+    moneda = parameters.get("criptomoneda")
+    url = "http://api.coinmarketcap.com/v1/ticker/" + moneda
+    speech = "Lo siento no encuentro el valor de " + moneda
     hora = datetime.now().strftime('%Y-%m-%d %H:%m')
     if accion == "mercados.bitcoin.precioactual":
-        moneda = parameters.get("criptomoneda")
-        url = url + moneda
-        request = urllib.request.Request(url)
-        response = urllib.request.urlopen(request)
-        preciobtc = json.loads(response.read())
-        speech[0] = "Tomado de http://api.coinmarketcap.com/v1/ticker/"
-        speech[1] = "Actualizado el: " + hora
-        speech[2] = "Precio USD de:" + moneda + " " + str(preciobtc[0]['price_usd']) + "$"
+        try:
+            request = urllib.request.Request(url)
+            response = urllib.request.urlopen(request)
+            preciobtc = json.loads(response.read())
+            speech = "Tomado de " + url +\
+                     "\n Actualizado el: " + hora +\
+                     "\n Precio USD de:" + moneda + " " + str(preciobtc[0]['price_usd']) + "$"
+        except urllib.error.URLError:
+            speech = "No se ha podido acceder a " + url
     return speech
 
 # $curl -d '{"result":{"action":"mercados.bitcoin.precioactual","parameters":{"criptomoneda":"Bitcoin"}}}' http://localhost:8080/webhook
