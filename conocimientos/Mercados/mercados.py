eea216453045599b08eda1c7e0e8e44fb97deb3e
yoidel <yoidel86@gmail.com>
2017-12-29 10:31:19 -0500

    agregando estado HAU

--- conocimientos/Mercados/mercados.py
+++ conocimientos/Mercados/mercados.py
@@ -17,4 +17,23 @@ def intradia(accion,parameters):
         speech = "El precio actual de "+str(intradia["symbol"])+" es $"+str(intradia["price"])+", con un cambio de "+\
                  str(intradia["percent"])+" y un volumen de "+str(intradia["volume"])+". Obtenido el "+str(intradia["date"])+" a las "+intradia["time"]
     return speech
+
+def haustatus(_,parameters):
+    mercado = parameters.get("mercados")
+    url_api = "http://h-kont.herokuapp.com/api/fstatus"
+    status = False
+    speech = "Lo siento no puedo obtener el estado "
+    try:
+        with urllib.request.urlopen(url_api) as url:
+            data = json.loads(url.read().decode())
+            status = data["data"]
+            print(data["data"])
+    except:
+        status = False
+    if (status):
+        speech = "El ultimo mensaje recibido por la HAU fue a las " + str(status["Time_last_message"]) + " de " + str(
+            status["Date"])
+    return speech
+
 #curl -d '{"result":{"action":"conocimientos.mercados.delmes","parameters":{"meses":"Julio"}}}' http://localhost:8080/webhook
+#curl -d '{"result":{"action":"conocimientos.mercados.haustatus"}}' http://localhost:8080/webhook
